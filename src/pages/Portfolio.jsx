ce8676aac91bd73b9164973559b6275899be5416
Rafael Gomez <rgomez2014@gmail.com>
2023-11-12 11:21:07 -0600

    Getting portfolio up

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -1,1 +1,18 @@
-imp
+import Projects from "../components/Projects";
+import projectData from  "../utils/projectData.json"
+
+// const Data = () =>{
+//   const [result, setResult] = useState([]);
+
+// } 
+
+const Portfolio = () => {
+  return(
+    <div>
+      {projectData.map((projects, index) => (
+        <Projects key={index} {...projects} />
+      ))}
+    </div>
+  )
+}
+export default Portfolio;
