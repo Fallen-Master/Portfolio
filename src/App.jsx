6cbe539a72aa3f044cf3d8cd5da408f620240cba
Rafael Gomez <rgomez2014@gmail.com>
2023-11-12 11:20:48 -0600

    Part of renaming

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 
 import { Outlet } from 'react-router-dom';
-import Header from './componets/Header'
-import Footer from './componets/Footer'
+import Header from './components/Header'
+import Footer from './components/Footer'
 
 
 function App() {
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 
 import ReactDOM from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import { Bulma } from 'react-bulma'
+// import { Bulma } from 'react-bulma'
 
 import 'bulma/css/bulma.min.css'
 import './index.css'
@@ -9,26 +9,30 @@ import './index.css'
 
 import App from './App.jsx'
 import AboutMe from './pages/AboutMe'
+import Portfolio from './pages/Portfolio'
 // import Contact from './pages/Contact'
-// import Portfolio from './pages/Portfolio'
 // import Resume from './pages/Resume'
 // import Error from './pages/Error'
 
 
 const router = createBrowserRouter([
   {
-    path: '/AboutME',
+    path: '/',
     element: <App />,
-    errorElement: <Error />,
+    // errorElement: <Error />,
     children: [
       {
         index: true,
         element: <AboutMe />,
       },
-      // {
-      //   path: '/Portfolio',
-      //   element: <Portfolio />,
-      // },
+      {
+        path: '/AboutMe',
+        element:<AboutMe />
+      },
+      {
+        path: '/Portfolio',
+        element: <Portfolio />,
+      },
       // {
       //   path: '/Contact',
       //   element: <Contact />,
