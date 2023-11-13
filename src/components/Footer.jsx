24d7c050904f5761fd7f664c5e66d04a8e8a34fa
Rafael Gomez <rgomez2014@gmail.com>
2023-11-12 22:37:21 -0600

    CSS update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,5 @@
-import githubWhiteMark from '../image/github-mark-white.png';
-// import githubMark from '../image/github-mark.png'
+// import githubWhiteMark from '../image/github-mark-white.png';
+import githubMark from '../image/github-mark.png'
 import linkedInLogo from '../image/LI-In-Bug.png';
 
 import './footer.css'
@@ -7,23 +7,21 @@ import './footer.css'
 function Footer() {
 
   return (
-    <div className="wrapper">
-      {/* Your page content goes here */}
-      {/* You can place your header, main content, etc., above the footer */}
-      
-      <footer className="footer footerBottom" id='footer-color' role="contentinfo">
+    <div className="wrapper footer-color">
+      <footer className="" id='bottom-footer' role="contentinfo">
         <div className="content has-text-centered">
           <div className="icons is-flex is-justify-content-center">
-            <a href="https://github.com/Fallen-Master" target="_blank" rel="noopener noreferrer" className='icon mx-3'>
-              <img src={githubWhiteMark} alt="Github" />
-            </a>
             {/* <a href="https://github.com/Fallen-Master" target="_blank" rel="noopener noreferrer" className='icon mx-3'>
-              <img src={githubMark} alt="Github" />
+              <img src={githubWhiteMark} alt="Github" />
             </a> */}
+            <a href="https://github.com/Fallen-Master" target="_blank" rel="noopener noreferrer" className='icon mx-3'>
+              <img src={githubMark} alt="Github" />
+            </a>
             <a href="https://www.linkedin.com/in/rafael-gomez-4a82317b/" target="_blank" rel="noopener noreferrer" className='icon mx-3'>
               <img src={linkedInLogo} alt="LinkedIn" />
             </a>
           </div>
+          <div> Rafael Gomez</div>
         </div>
       </footer>
     </div>
